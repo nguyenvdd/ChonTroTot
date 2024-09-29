c7a810e285c8138f1e7ede5b465b7719e0b5f2af
nguyenvdd <nguyenvanduc2k3@gmail.com>
2024-09-29 16:49:38 +0700

    ChonTroTot

---
+++ client/src/pages/admin/index.jsx
@@ -0,0 +1,4 @@
+export { default as AdminLayout } from "./AdminLayout"
+export { default as ManagerUser } from "./ManageUser"
+export { default as AdminDashboard } from "./AdminDashboard"
+export { default as ManagerDashboard } from "./ManagerDashboard"
